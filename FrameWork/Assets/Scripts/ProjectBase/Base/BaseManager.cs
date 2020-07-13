006ae14817d5efae16975d28126c4a615c1a9cc4
MagicianThirteen <magicianthirteen@gmail.com>
2020-07-13 18:54:01 +0800

    缓存池简单版

--- FrameWork/Assets/Scripts/ProjectBase/Base/BaseManager.cs
+++ FrameWork/Assets/Scripts/ProjectBase/Base/BaseManager.cs
@@ -10,10 +10,7 @@ using System;
 public class BaseManage<T> where T:new()
 {
     private static T instance;
-    private BaseManage()
-    {
-
-    }
+  
     public static T GetInstance()
     {
         if (instance == null)
@@ -24,38 +21,4 @@ public class BaseManage<T> where T:new()
 
 }
 
-
-////加了线程安全和构造私有的写法
-//public class DyhSingleton<T> where T : class
-//{
-//    private static T instance;
-//    private static object initLock = new object();
-//    public static T GetInstance()
-//    {
-//        if (instance == null)
-//            CreateInstance();
-
-//        return instance;
-//    }
-//    private static void CreateInstance()
-//    {
-//        lock (initLock)
-//        {
-//            if (instance == null)
-//            {
-//                Type t = typeof(T);
-
-//                // Ensure there are no public constructors...
-//                // 这里确保没有其它的public构造函数了，既没有可以通过其它方法new这个类
-//                ConstructorInfo[] ctors = t.GetConstructors();
-//                if (ctors.Length > 0)
-//                {
-//                    throw new InvalidOperationException(String.Format("{0} has at least one accesible ctor making it impossibleto enforce DyhSingleton behaviour", t.Name));
-//                }
-//                // Create an instance via the private constructor
-//                instance = (T)Activator.CreateInstance(t, true);
-//            }
-//        }
-//    }
-//}
 
--- FrameWork/Assets/Scripts/ProjectBase/Pool/PoolMgr.cs
+++ FrameWork/Assets/Scripts/ProjectBase/Pool/PoolMgr.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Pool : MonoBehaviour
+/// <summary>
+/// 缓存池
+/// </summary>
+public class PoolMgr : BaseManage<PoolMgr>
 {
-    // Start is called before the first frame update
-    void Start()
+    //定义衣柜的容器
+    Dictionary<string, List<GameObject>> poolDic = new Dictionary<string, List<GameObject>>();
+    //定义缓存池借的行为
+    public GameObject GetObj(string name)
     {
-        
+        //分两种情况，1.有这个抽屉和抽屉有东西2.抽屉没东西
+        GameObject obj = null;
+        if (poolDic.ContainsKey(name) && poolDic[name].Count > 0 )//当这里containskey写后面时，就会报错，因为找不到这个key
+        {
+            
+            obj = poolDic[name][0];
+            poolDic[name].RemoveAt(0);
+        }
+        else
+        {
+            obj = GameObject.Instantiate(Resources.Load<GameObject>(name));
+        }
+        obj.SetActive(true);
+        return obj;
     }
-
-    // Update is called once per frame
-    void Update()
+    //定义缓存池还的行为(需要知道还到哪个抽屉，还了什么物体）
+    public void pushObj(string name,GameObject obj)
     {
-        
+        //还回来的时候失活
+        obj.SetActive(false);
+        //分两种情况：衣柜有这个抽屉，衣柜没有这个抽屉
+        if (poolDic.ContainsKey(name))
+        {
+            poolDic[name].Add(obj);
+        }
+        else
+        {
+            poolDic.Add(name, new List<GameObject> { obj });
+        }
     }
 }
