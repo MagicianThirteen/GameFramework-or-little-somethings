ad151f94aa4ba72379982bb5a6649f80f2857588
MagicianThirteen <magicianthirteen@gmail.com>
2020-09-04 10:18:34 +0800

    简单版本的对象池

--- FrameWork/Assets/Scripts/ProjectBase/Pool/PoolMgr.cs
+++ FrameWork/Assets/Scripts/ProjectBase/Pool/PoolMgr.cs
@@ -4,7 +4,48 @@ using UnityEngine;
 /// <summary>
 /// 缓存池
 /// </summary>
-public class PoolMgr 
+public class PoolMgr :BaseManager<PoolMgr>
 {
-   
+
+    Dictionary<string, List<GameObject>> poolDic = new Dictionary<string, List<GameObject>>();
+
+    /// <summary>
+    /// 往衣柜里放东西
+    /// </summary>
+    /// <param name="name">衣柜名字</param>
+    /// <returns>游戏物体</returns>
+    public GameObject GetObj(string name)
+    {
+        GameObject obj = null;
+        if (poolDic.ContainsKey(name) && poolDic[name].Count > 0)
+        {
+            obj = poolDic[name][0];
+            poolDic[name].RemoveAt(0);
+        }
+        else
+        {
+            obj = GameObject.Instantiate(Resources.Load<GameObject>(name));
+        }
+        
+        obj.SetActive(true);
+        return obj;
+    }
+
+    /// <summary>
+    /// 往衣柜添加东西
+    /// </summary>
+    /// <param name="name">衣柜名字</param>
+    /// <param name="obj">衣柜里的东西</param>
+    public void PushObj(string name,GameObject obj)
+    {
+        obj.SetActive(false);
+        if (!poolDic.ContainsKey(name))
+        {
+            poolDic.Add(name, new List<GameObject>() { obj});
+        }
+        else
+        {
+            poolDic[name].Add(obj);
+        }
+    }
 }
